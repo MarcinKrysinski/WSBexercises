b28b86bd2eda38dbfa1600cca051d5c09d4d7461
Marcin <marcin.jakubkrysinski@gmail.com>
2021-01-18 21:51:30 +0100

    Task 5

--- src/main/java/pl/krysinski/Car.java
+++ src/main/java/pl/krysinski/Car.java
@@ -3,9 +3,15 @@ package pl.krysinski;
 public class Car {
     private final String producer;
     private final String model;
+    private Double value;
 
-    public Car(String producer, String model) {
+    public Car(String producer, String model, Double value) {
         this.producer = producer;
         this.model = model;
+        this.value = value;
+    }
+
+    public Double getValue() {
+        return value;
     }
 }
--- src/main/java/pl/krysinski/Human.java
+++ src/main/java/pl/krysinski/Human.java
@@ -23,12 +23,28 @@ public class Human {
         return salary;
     }
 
+    public Car getCar() {
+        return car;
+    }
+
     public void setSalary(Double salary) {
         if(salary < 0){
             System.out.println("Wypłata nie może być ujemna!!!");
         }else{
             this.salary = salary;
             System.out.println("Zmieniono wysokośc pensji! Aktualana pensja wynosi: " + salary);
+        }
+    }
+
+    public void setCar(Car car) {
+        if(this.salary > car.getValue()){
+            System.out.println("Kupiłeś auto za gotówke!");
+            this.car = car;
+        }else if (this.salary > car.getValue()/12){
+            System.out.println("Wziąłeś auto na kredyt!");
+            this.car = car;
+        }else{
+            System.out.println("Nie stać Cię lamusie, skończ studia albo jedź na truskawki czy coś!");
         }
     }
 
